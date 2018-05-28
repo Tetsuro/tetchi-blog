9a3f55056f5555acb8546cbb0270dbcba59907d8
Tetchi Takara <tetchi@Tetchis-Mac-mini.local>
2018-05-27 23:47:46 -0400

    Add a filter to hide PW-protected posts

--- functions.php
+++ functions.php
@@ -9,4 +9,14 @@
   }
 
   add_action('after_setup_theme', 'mytheme_setup_theme_supported_features');
+
+  function wpb_password_post_filter( $where = '' ) {
+    if (!is_single() && !is_admin()) {
+        $where .= " AND post_password = ''";
+    }
+    return $where;
+  }
+  
+  add_filter( 'posts_where', 'wpb_password_post_filter' );
+
 ?>
